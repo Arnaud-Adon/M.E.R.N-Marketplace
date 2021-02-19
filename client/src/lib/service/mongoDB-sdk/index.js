48dff165e32fc5a91c12a0160f80f6d7687d677b
Arnaud-Adon <assy.adon@yahoo.fr>
2021-02-19 01:53:45 +0100

    Realm app id added

--- client/src/lib/service/mongoDB-sdk/index.js
+++ client/src/lib/service/mongoDB-sdk/index.js
@@ -1,4 +1,4 @@
 import * as Realm from "realm-web";
 
-const REALM_APP_ID = process.env.REALM_APP_ID;
+const REALM_APP_ID = "application-marketplace-fzrzz";
 export const realmApp = new Realm.App({ id: REALM_APP_ID });
